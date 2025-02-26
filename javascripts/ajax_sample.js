a91916a71140a4d374a27cccad5c0563493e615d
kelvinquansah <kelvinquansah477@gmail.com>
2025-02-26 08:10:46 +0000

    second commit

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,32 +1,62 @@
 let number = 0;
 
-let titleArea = document.getElementById("title")
-let contentArea = document.getElementById("content")
-let videoArea = document.getElementById("video")
+const titleArea = document.getElementById("title")
+const contentArea = document.getElementById("content")
+const videoArea = document.getElementById("video")
+const button = document.getElementById("btn")
 
-let button = document.getElementById("btn")
+let data = [];
+
+// function getData() {
+//     button.addEventListener("click", () => {
+
+//         // 1. Generate XMLHttpRequest object
+//         const request = new XMLHttpRequest(); //-- 1
+//         // Set event handlers in the XMLHttpRequest object and configure processing during communication
+//         request.onreadystatechange = function () { //-- 2-1
+//             if (request.readyState == 4) { //-- 2-2
+//                 if (request.status == 200) {
+//                     titleArea.innerHTML = request.response[number].title;
+//                     contentArea.innerHTML = request.response[number].content;
+//                     videoArea.setAttribute("src", request.response[number].url);
+//                     number = number == request.response.length - 1 ? 0 : (number + 1)
+//                 }
+//             }
+//         };
+//         // Send request
+//         request.open("GET", "ajax.json"); //-- 3-1
+//         request.responseType = "json"; //-- 3-2
+//         request.send(null); //-- 3-3
+//     });
+// }
 
 function getData() {
-    button.addEventListener("click", () => {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
+            if (request.status == 200) {
+                data = request.response
 
-        // 1. Generate XMLHttpRequest object
-        const request = new XMLHttpRequest(); //-- 1
-        // Set event handlers in the XMLHttpRequest object and configure processing during communication
-        request.onreadystatechange = function () { //-- 2-1
-            if (request.readyState == 4) { //-- 2-2
-                if (request.status == 200) {
-                    titleArea.innerhtml = request.response[number].title;
-                    contentArea.innerhtml = request.response[number].content;
-                    videoArea.setAttribute("src", request.response[number].url);
-                    number = number == request.response.length - 1 ? 0 : (number + 1)
-                }
             }
-        };
-        // Send request
-        request.open("GET", "ajax.json"); //-- 3-1
-        request.responseType = "json"; //-- 3-2
-        request.send(null); //-- 3-3
+        }
+    };
+    request.open("GET", "ajax.json");
+    request.responseType = "json";
+    request.send(null);
+}
+function changeVideo() {
+
+    button.addEventListener("click", (e) => {
+        if (data.length == 0) {
+            getData()
+        }
+        titleArea.innerHTML = data[number].title;
+        contentArea.innerHTML = data[number].content;
+        videoArea.setAttribute("src", data[number].url);
+        number == 2 ? (number = 0) : number++;
     });
 }
 
-window.onload = getData; 
+
+
+window.onload = changeVideo; 
